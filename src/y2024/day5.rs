5c2b6d4e365ba23db71630d81a71a39d3ae97ebb
Thomas Roberts <thomas.roberts@automattic.com>
2024-12-05 17:07:30 +0000

    Update to use Vec instead of HashSet

--- src/y2024/day5.rs
+++ src/y2024/day5.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap};
 use itertools::Itertools;
 use crate::input_reader::input_reader::read_input_for_day;
 
@@ -12,7 +12,7 @@ pub fn task() {
     println!("Part 2: {} in {:?}", part2( input.clone() ), start.elapsed());
 }
 
-fn parse_input( input: String ) -> ( Vec<Vec<usize>>, HashMap<usize,HashSet<usize>> ) {
+fn parse_input( input: String ) -> ( Vec<Vec<usize>>, HashMap<usize,Vec<usize>> ) {
     let ( ordering_rules_raw, updates_raw ): (&str, &str) = input.split_once("\n\n" ).unwrap();
     
     let ordering_rules: Vec<(usize, usize)> = ordering_rules_raw
@@ -32,15 +32,15 @@ fn parse_input( input: String ) -> ( Vec<Vec<usize>>, HashMap<usize,HashSet<usiz
 
     let page_map = ordering_rules
         .iter()
-        .fold( HashMap::<usize,HashSet<usize>>::new(), | mut map, val | {
+        .fold( HashMap::<usize,Vec<usize>>::new(), | mut map, val | {
             let ( before, after ) = val;
             let mut must_come_before = map
                 .get( before )
-                .or( Some( &HashSet::new() ) )
+                .or( Some( &Vec::new() ) )
                 .unwrap()
                 .clone();
 
-            must_come_before.insert( *after );
+            must_come_before.push( *after );
             map.insert( *before, must_come_before );
             map
         } );
@@ -50,7 +50,7 @@ fn parse_input( input: String ) -> ( Vec<Vec<usize>>, HashMap<usize,HashSet<usiz
 
 fn part1( input: String ) -> usize {
     let ( updates, page_map ) = parse_input( input );
-    let empty_set = HashSet::<usize>::new();
+    let empty_set = Vec::<usize>::new();
     let mut total = 0;
     for update in updates {
         let mut valid = true;
@@ -58,9 +58,9 @@ fn part1( input: String ) -> usize {
             if ! valid {
                 continue;
             }
-            let pages_before: HashSet<&usize> = update.iter().take( index ).collect();
+            let pages_before: Vec<&usize> = update.iter().take( index ).collect();
             let must_come_before = page_map.get( page ).unwrap_or( &empty_set );
-            valid = ! must_come_before.iter().any( | page | pages_before.contains( page ) );
+            valid = ! must_come_before.iter().any( | page | pages_before.contains( &&page ) );
         }
         if valid {
             let middle_page = update.get( update.len() / 2 ).unwrap();
@@ -72,7 +72,7 @@ fn part1( input: String ) -> usize {
 
 fn part2( input: String ) -> usize {
     let ( updates, page_map ) = parse_input( input );
-    let empty_set = HashSet::<usize>::new();
+    let empty_set = Vec::<usize>::new();
     let mut total = 0;
     let invalid_updates: Vec<Vec<usize>> = updates.iter().filter_map( | update | {
         let mut valid = true;
@@ -80,9 +80,9 @@ fn part2( input: String ) -> usize {
             if ! valid {
                 continue;
             }
-            let pages_before: HashSet<&usize> = update.iter().take( index ).collect();
+            let pages_before: Vec<&usize> = update.iter().take( index ).collect();
             let must_come_before = page_map.get( page ).unwrap_or( &empty_set );
-            valid = ! must_come_before.iter().any( | page | pages_before.contains( page ) );
+            valid = ! must_come_before.iter().any( | page | pages_before.contains( &&page ) );
         }
         match valid {
             true => None,
@@ -96,7 +96,7 @@ fn part2( input: String ) -> usize {
         let mut new_update = Vec::<usize>::new();
         for page in update {
             let must_come_before = page_map.get( page ).unwrap_or( &empty_set );
-            let pages_before: HashSet<&usize> = new_update.iter().collect();
+            let pages_before: Vec<&usize> = new_update.iter().collect();
             let ( insert_at, _ ) = new_update.iter().find_position( | page | must_come_before.contains( page ) ).unwrap_or( ( pages_before.len(), &0 ) );
             new_update.insert( insert_at, page.clone() );
         }
